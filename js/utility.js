02e4c06bea4c6e8ac1b06022db6de1807464d0e1
RakeshSR10 <rakeshsrking@gmail.com>
2021-05-28 00:08:54 +0530

    Added: Populate EmpPayroll Class with json Obj & save to localStorage

--- js/utility.js
+++ js/utility.js
@@ -4,3 +4,11 @@ const stringifyDate = (date) => {
                     new Date(Date.parse(date)).toLocalDateString('en-GB', options);
     return newDate;                
 }
+
+//UC 13 - Ability to Update Employee Payroll Details from Home Page
+const update = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._id == node._id);
+    if (!empPayrollData) return;
+    localStorage.setItem('editEmp', JSON.stringify(empPayrollData));
+    window.location.replace(site_properties.add_emp_payroll_page);
+}
