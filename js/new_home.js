686279fea1b5134ca1ac283c711cce4e063167d7
RakeshSR10 <rakeshsrking@gmail.com>
2021-05-27 21:38:57 +0530

    Added: Ability to delete Employee details from App

--- js/new_home.js
+++ js/new_home.js
@@ -1,9 +1,10 @@
-//UC 11
+/* UC 11*/
 let empPayrollList;//global variable
 window.addEventListener('DOMContentLoaded', (event) => {
     empPayrollList = getEmployeePayrollDataFromStorage();
     document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
 
 const getEmployeePayrollDataFromStorage = () => {
@@ -12,29 +13,30 @@ const getEmployeePayrollDataFromStorage = () => {
 }
 
 //UC 9 - View Employee Payroll Details in a Tabular Format from JS File
-createInnerHtml = () => {
+const createInnerHtml = () => {
     if (empPayrollList.length == 0) return;
-
     const headerHtml =  "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
     //UC 10 set innerHtml                   
     let innerHtml = `${headerHtml}`;
     //let employeePayrollList = createEmployeePayrollJSON();
+
     //UC 10 -for loop for printing multiple employee
     for(const empPayrollData of employeePayrollList) {
         innerHtml = `${innerHtml}
         <tr>
-            <td><img class="progile" src="${empPayrollData._profilePic}" alt=""></td>
+            <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
                 <td>${empPayrollData._name}</td>
                 <td>${empPayrollData._gender}</td>
                 <td>${getDeptHtml(empPayrollData._department)}</td>
                 <td>${empPayrollData._salary}</td>
-                <td>${empPayrollData._startDate}</td>
+                <td>${stringifyDate(empPayrollData._startDate)}</td>
                 <td>
-                    <img name="${empPayrollData._id}" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
-                    <img name="${empPayrollData._id}" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
+                    <img id="${empPayrollData._id}" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
+                    <img id="${empPayrollData._id}" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
                 </td>    
-        </tr> `;
+        </tr>
+        `;
     }
     document.querySelector('#table-display').innerHTML = innerHtml;
 }
@@ -77,4 +79,16 @@ const getDeptHtml = (deptList) => {
         deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
     }
     return deptHtml;
+}
+
+//UC - 12 Remove Data From Employee Details.
+const remove = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._id == node._id);
+    if (!empPayrollData) return;
+    const index = empPayrollList.map(empData => empData._id)
+                                .indexOf(empPayrollData._id);
+    empPayrollList.splice(index, 1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHtml();
 }
